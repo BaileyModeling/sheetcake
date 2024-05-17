2219e6e06ce81e943475803b43a0a7f0fbda784b
BaileyModeling <BaileyModeling@users.noreply.github.com>
2024-05-17 10:34:24 -0500

    replaced max_date_cell with DateCell.max classmethod

--- src/cells/date_cell.py
+++ src/cells/date_cell.py
@@ -272,39 +272,26 @@ class DateCell:
         new_cell.eomonth_item(cell=date_cell, num_months=num_months, update=True)
         return new_cell
 
+    @classmethod
+    def max(
+        cls,
+        cells: List["DateCell"],
+        name: str = "<DateCell>",
+        fmt: Callable = fmt.mmddyyyy,
+        callback: Callable = None,
+        locked: bool = False,
+        validation_rules: List[Callable] = None
+    ) -> "DateCell":
+        new_cell = cls(value=None, name=name, fmt=fmt, callback=callback, locked=locked, validation_rules=validation_rules)
+        new_cell.max_items(cells=cells, update=True)
+        return new_cell
+
 
 def get_value(obj) -> date:
     if hasattr(obj, 'value'):
         return obj.value
     else:
         return obj
-
-
-# def eomonth_cell(
-#     date_cell: DateCell,
-#     num_months: int = 0,
-#     name: str = "<DateCell>",
-#     fmt: Callable = fmt.mmddyyyy,
-#     callback: Callable = None,
-#     locked: bool = False,
-#     validation_rules: List[Callable] = None
-# ) -> DateCell:
-#     new_cell = DateCell(value=None, name=name, fmt=fmt, callback=callback, locked=locked, validation_rules=validation_rules)
-#     new_cell.eomonth_item(cell=date_cell, num_months=num_months, update=True)
-#     return new_cell
-
-
-def max_date_cell(
-    cells: List[DateCell],
-    name: str = "<DateCell>",
-    fmt: Callable = fmt.mmddyyyy,
-    callback: Callable = None,
-    locked: bool = False,
-    validation_rules: List[Callable] = None
-) -> DateCell:
-    new_cell = DateCell(value=None, name=name, fmt=fmt, callback=callback, locked=locked, validation_rules=validation_rules)
-    new_cell.max_items(cells=cells, update=True)
-    return new_cell
 
 
 def min_date_cell(
--- tests/date_cell/test_date_cell.py
+++ tests/date_cell/test_date_cell.py
@@ -1,5 +1,5 @@
 from sheetcake import DateCell, errors, validation_rules
-from sheetcake.src.cells.date_cell import max_date_cell, min_date_cell, AbstractDateOperation
+from sheetcake.src.cells.date_cell import min_date_cell, AbstractDateOperation
 from datetime import date
 import pytest
 from typing import List
@@ -155,7 +155,7 @@ def test_max_date_cell():
     a = DateCell(date(2000, 1, 1), "a")
     b = DateCell(date(2024, 1, 1), "b")
     c = DateCell(date(2025, 1, 1), "c")
-    d = max_date_cell([a, b, c], name="d")
+    d = DateCell.max([a, b, c], name="d")
     assert d.value == date(2025, 1, 1)
 
 
@@ -339,12 +339,12 @@ def test_date_cell_formula_deep_eomonth_edays():
 
 
 def test_date_cell_max_formula(cell_list: List[DateCell]):
-    e = max_date_cell(cells=cell_list, name="e")
+    e = DateCell.max(cells=cell_list, name="e")
     assert e.formula() == "max( a, b, c, d )"
 
 
 def test_date_cell_max_formula_deep(cell_list: List[DateCell]):
-    e = max_date_cell(cells=cell_list, name="e")
+    e = DateCell.max(cells=cell_list, name="e")
     assert e.formula(deep=True) == "max( 2024-01-01, 2025-01-01, 2026-01-01, 2027-01-01 )"
 
 
@@ -395,12 +395,12 @@ def test_date_cell_edate_max_combined_formula_deep():
 
 
 def test_date_cell_max_value_audit(cell_list: List[DateCell]):
-    e = max_date_cell(cells=cell_list, name="e")
+    e = DateCell.max(cells=cell_list, name="e")
     assert e.value_audit() == "max( a, b, c, d )"
 
 
 def test_date_cell_max_value_audit_deep(cell_list: List[DateCell]):
-    e = max_date_cell(cells=cell_list, name="e")
+    e = DateCell.max(cells=cell_list, name="e")
     assert e.value_audit(deep=True) == "max( 2024-01-01, 2025-01-01, 2026-01-01, 2027-01-01 )"
 
 
